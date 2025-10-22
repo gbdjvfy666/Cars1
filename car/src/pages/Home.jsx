6163be15d1988d403ebf5da6690dacc7e438e25f
Lepeha <nostresko@gmail.com>
2025-10-22 23:23:47 +0400

    флаги

--- car/src/pages/Home.jsx
+++ car/src/pages/Home.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ThreeScene from '../components/ThreeScene';
 import DesignCard from '../components/DesignCard';
 import HeroSection from '../components/HeroSection';
-import PortholeUI from '../components/PortholeUI';
+
 import SearchPanelUI from '../components/SearchPanelUI';
 import '../index.css';
 export default function Home() {
   return (
     <div>
         <HeroSection />
-        <div className="porthole-section">
-            <PortholeUI />
-        </div>
+
         <SearchPanelUI/>
         <DesignCard />
         <ThreeScene />
